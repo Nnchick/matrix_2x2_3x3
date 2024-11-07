5aa2dd78533b0bb9064b0e365237499cdf849c3c
gugla <gugla@NNCHICK>
2024-11-08 02:04:35 +0300

    Добавьте файлы проекта.

---
+++ 5566/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace _5566
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtB1 = new System.Windows.Forms.TextBox();
+            this.txtA2 = new System.Windows.Forms.TextBox();
+            this.txtC2 = new System.Windows.Forms.TextBox();
+            this.txtA1 = new System.Windows.Forms.TextBox();
+            this.txtB2 = new System.Windows.Forms.TextBox();
+            this.txtC1 = new System.Windows.Forms.TextBox();
+            this.txtC2_3x3 = new System.Windows.Forms.TextBox();
+            this.txtB2_3x3 = new System.Windows.Forms.TextBox();
+            this.txtA2_3x3 = new System.Windows.Forms.TextBox();
+            this.txtD1_3x3 = new System.Windows.Forms.TextBox();
+            this.txtC1_3x3 = new System.Windows.Forms.TextBox();
+            this.txtB1_3x3 = new System.Windows.Forms.TextBox();
+            this.txtA1_3x3 = new System.Windows.Forms.TextBox();
+            this.txtD3_3x3 = new System.Windows.Forms.TextBox();
+            this.txtC3_3x3 = new System.Windows.Forms.TextBox();
+            this.txtB3_3x3 = new System.Windows.Forms.TextBox();
+            this.txtA3_3x3 = new System.Windows.Forms.TextBox();
+            this.txtD2_3x3 = new System.Windows.Forms.TextBox();
+            this.lblResult2x2 = new System.Windows.Forms.Label();
+            this.btnSolve2x2 = new System.Windows.Forms.Button();
+            this.btnSolve3x3 = new System.Windows.Forms.Button();
+            this.lblResult3x3 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtB1
+            // 
+            this.txtB1.Location = new System.Drawing.Point(42, 41);
+            this.txtB1.Name = "txtB1";
+            this.txtB1.Size = new System.Drawing.Size(56, 22);
+            this.txtB1.TabIndex = 0;
+           
+            // 
+            // txtA2
+            // 
+            this.txtA2.Location = new System.Drawing.Point(42, 94);
+            this.txtA2.Name = "txtA2";
+            this.txtA2.Size = new System.Drawing.Size(56, 22);
+            this.txtA2.TabIndex = 1;
+            // 
+            // txtC2
+            // 
+            this.txtC2.Location = new System.Drawing.Point(254, 94);
+            this.txtC2.Name = "txtC2";
+            this.txtC2.Size = new System.Drawing.Size(56, 22);
+            this.txtC2.TabIndex = 2;
+            // 
+            // txtA1
+            // 
+            this.txtA1.Location = new System.Drawing.Point(143, 41);
+            this.txtA1.Name = "txtA1";
+            this.txtA1.Size = new System.Drawing.Size(56, 22);
+            this.txtA1.TabIndex = 3;
+            // 
+            // txtB2
+            // 
+            this.txtB2.Location = new System.Drawing.Point(143, 94);
+            this.txtB2.Name = "txtB2";
+            this.txtB2.Size = new System.Drawing.Size(56, 22);
+            this.txtB2.TabIndex = 4;
+            // 
+            // txtC1
+            // 
+            this.txtC1.Location = new System.Drawing.Point(254, 41);
+            this.txtC1.Name = "txtC1";
+            this.txtC1.Size = new System.Drawing.Size(56, 22);
+            this.txtC1.TabIndex = 5;
+            // 
+            // txtC2_3x3
+            // 
+            this.txtC2_3x3.Location = new System.Drawing.Point(633, 94);
+            this.txtC2_3x3.Name = "txtC2_3x3";
+            this.txtC2_3x3.Size = new System.Drawing.Size(56, 22);
+            this.txtC2_3x3.TabIndex = 6;
+            // 
+            // txtB2_3x3
+            // 
+            this.txtB2_3x3.Location = new System.Drawing.Point(535, 94);
+            this.txtB2_3x3.Name = "txtB2_3x3";
+            this.txtB2_3x3.Size = new System.Drawing.Size(56, 22);
+            this.txtB2_3x3.TabIndex = 7;
+            // 
+            // txtA2_3x3
+            // 
+            this.txtA2_3x3.Location = new System.Drawing.Point(437, 94);
+            this.txtA2_3x3.Name = "txtA2_3x3";
+            this.txtA2_3x3.Size = new System.Drawing.Size(56, 22);
+            this.txtA2_3x3.TabIndex = 8;
+            // 
+            // txtD1_3x3
+            // 
+            this.txtD1_3x3.Location = new System.Drawing.Point(737, 41);
+            this.txtD1_3x3.Name = "txtD1_3x3";
+            this.txtD1_3x3.Size = new System.Drawing.Size(56, 22);
+            this.txtD1_3x3.TabIndex = 9;
+            // 
+            // txtC1_3x3
+            // 
+            this.txtC1_3x3.Location = new System.Drawing.Point(633, 41);
+            this.txtC1_3x3.Name = "txtC1_3x3";
+            this.txtC1_3x3.Size = new System.Drawing.Size(56, 22);
+            this.txtC1_3x3.TabIndex = 10;
+            // 
+            // txtB1_3x3
+            // 
+            this.txtB1_3x3.Location = new System.Drawing.Point(535, 41);
+            this.txtB1_3x3.Name = "txtB1_3x3";
+            this.txtB1_3x3.Size = new System.Drawing.Size(56, 22);
+            this.txtB1_3x3.TabIndex = 11;
+            // 
+            // txtA1_3x3
+            // 
+            this.txtA1_3x3.Location = new System.Drawing.Point(437, 41);
+            this.txtA1_3x3.Name = "txtA1_3x3";
+            this.txtA1_3x3.Size = new System.Drawing.Size(56, 22);
+            this.txtA1_3x3.TabIndex = 12;
+            // 
+            // txtD3_3x3
+            // 
+            this.txtD3_3x3.Location = new System.Drawing.Point(737, 157);
+            this.txtD3_3x3.Name = "txtD3_3x3";
+            this.txtD3_3x3.Size = new System.Drawing.Size(56, 22);
+            this.txtD3_3x3.TabIndex = 13;
+            // 
+            // txtC3_3x3
+            // 
+            this.txtC3_3x3.Location = new System.Drawing.Point(633, 157);
+            this.txtC3_3x3.Name = "txtC3_3x3";
+            this.txtC3_3x3.Size = new System.Drawing.Size(56, 22);
+            this.txtC3_3x3.TabIndex = 14;
+            // 
+            // txtB3_3x3
+            // 
+            this.txtB3_3x3.Location = new System.Drawing.Point(535, 157);
+            this.txtB3_3x3.Name = "txtB3_3x3";
+            this.txtB3_3x3.Size = new System.Drawing.Size(56, 22);
+            this.txtB3_3x3.TabIndex = 15;
+            // 
+            // txtA3_3x3
+            // 
+            this.txtA3_3x3.Location = new System.Drawing.Point(437, 157);
+            this.txtA3_3x3.Name = "txtA3_3x3";
+            this.txtA3_3x3.Size = new System.Drawing.Size(56, 22);
+            this.txtA3_3x3.TabIndex = 16;
+            // 
+            // txtD2_3x3
+            // 
+            this.txtD2_3x3.Location = new System.Drawing.Point(737, 94);
+            this.txtD2_3x3.Name = "txtD2_3x3";
+            this.txtD2_3x3.Size = new System.Drawing.Size(56, 22);
+            this.txtD2_3x3.TabIndex = 17;
+            // 
+            // lblResult2x2
+            // 
+            this.lblResult2x2.AutoSize = true;
+            this.lblResult2x2.Location = new System.Drawing.Point(140, 143);
+            this.lblResult2x2.Name = "lblResult2x2";
+            this.lblResult2x2.Size = new System.Drawing.Size(77, 16);
+            this.lblResult2x2.TabIndex = 18;
+            this.lblResult2x2.Text = "Результат";
+            // 
+            // btnSolve2x2
+            // 
+            this.btnSolve2x2.Location = new System.Drawing.Point(142, 188);
+            this.btnSolve2x2.Name = "btnSolve2x2";
+            this.btnSolve2x2.Size = new System.Drawing.Size(75, 23);
+            this.btnSolve2x2.TabIndex = 19;
+            this.btnSolve2x2.Text = "Решить";
+            this.btnSolve2x2.UseVisualStyleBackColor = true;
+            this.btnSolve2x2.Click += new System.EventHandler(this.btnSolve2x2_Click_1);
+            // 
+            // btnSolve3x3
+            // 
+            this.btnSolve3x3.Location = new System.Drawing.Point(598, 251);
+            this.btnSolve3x3.Name = "btnSolve3x3";
+            this.btnSolve3x3.Size = new System.Drawing.Size(75, 23);
+            this.btnSolve3x3.TabIndex = 20;
+            this.btnSolve3x3.Text = "Решить";
+            this.btnSolve3x3.UseVisualStyleBackColor = true;
+            this.btnSolve3x3.Click += new System.EventHandler(this.btnSolve3x3_Click_1);
+            // 
+            // lblResult3x3
+            // 
+            this.lblResult3x3.AutoSize = true;
+            this.lblResult3x3.Location = new System.Drawing.Point(595, 208);
+            this.lblResult3x3.Name = "lblResult3x3";
+            this.lblResult3x3.Size = new System.Drawing.Size(77, 16);
+            this.lblResult3x3.TabIndex = 21;
+            this.lblResult3x3.Text = "Результат";
+           
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(387, 445);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 22;
+            this.btnClose.Text = "Закрыть";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click_1);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(866, 564);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblResult3x3);
+            this.Controls.Add(this.btnSolve3x3);
+            this.Controls.Add(this.btnSolve2x2);
+            this.Controls.Add(this.lblResult2x2);
+            this.Controls.Add(this.txtD2_3x3);
+            this.Controls.Add(this.txtA3_3x3);
+            this.Controls.Add(this.txtB3_3x3);
+            this.Controls.Add(this.txtC3_3x3);
+            this.Controls.Add(this.txtD3_3x3);
+            this.Controls.Add(this.txtA1_3x3);
+            this.Controls.Add(this.txtB1_3x3);
+            this.Controls.Add(this.txtC1_3x3);
+            this.Controls.Add(this.txtD1_3x3);
+            this.Controls.Add(this.txtA2_3x3);
+            this.Controls.Add(this.txtB2_3x3);
+            this.Controls.Add(this.txtC2_3x3);
+            this.Controls.Add(this.txtC1);
+            this.Controls.Add(this.txtB2);
+            this.Controls.Add(this.txtA1);
+            this.Controls.Add(this.txtC2);
+            this.Controls.Add(this.txtA2);
+            this.Controls.Add(this.txtB1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.TextBox textBox14;
+        private System.Windows.Forms.TextBox textBox15;
+        private System.Windows.Forms.TextBox textBox16;
+        private System.Windows.Forms.TextBox textBox17;
+        private System.Windows.Forms.TextBox textBox18;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtB1;
+        private System.Windows.Forms.TextBox txtA2;
+        private System.Windows.Forms.TextBox txtC2;
+        private System.Windows.Forms.TextBox txtA1;
+        private System.Windows.Forms.TextBox txtB2;
+        private System.Windows.Forms.TextBox txtC1;
+        private System.Windows.Forms.TextBox txtC2_3x3;
+        private System.Windows.Forms.TextBox txtB2_3x3;
+        private System.Windows.Forms.TextBox txtA2_3x3;
+        private System.Windows.Forms.TextBox txtD1_3x3;
+        private System.Windows.Forms.TextBox txtC1_3x3;
+        private System.Windows.Forms.TextBox txtB1_3x3;
+        private System.Windows.Forms.TextBox txtA1_3x3;
+        private System.Windows.Forms.TextBox txtD3_3x3;
+        private System.Windows.Forms.TextBox txtC3_3x3;
+        private System.Windows.Forms.TextBox txtB3_3x3;
+        private System.Windows.Forms.TextBox txtA3_3x3;
+        private System.Windows.Forms.TextBox txtD2_3x3;
+        private System.Windows.Forms.Label lblResult2x2;
+        private System.Windows.Forms.Button btnSolve2x2;
+        private System.Windows.Forms.Button btnSolve3x3;
+        private System.Windows.Forms.Label lblResult3x3;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ 5566/Form1.cs
@@ -0,0 +1,87 @@
+﻿
+using System;
+using System.Windows.Forms;
+
+namespace _5566
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSolve2x2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                double a1 = Convert.ToDouble(txtB1.Text);
+                double b1 = Convert.ToDouble(txtA2.Text);
+                double c1 = Convert.ToDouble(txtC1.Text);
+                double a2 = Convert.ToDouble(txtA2.Text);
+                double b2 = Convert.ToDouble(txtB2.Text);
+                double c2 = Convert.ToDouble(txtC2.Text);
+
+                double det = a1 * b2 - a2 * b1;
+                if (det == 0)
+                {
+                    MessageBox.Show("Система не имеет единственного решения.");
+                    return;
+                }
+
+                double x = (c1 * b2 - c2 * b1) / det;
+                double y = (a1 * c2 - a2 * c1) / det;
+
+                lblResult2x2.Text = $"x = {x}, y = {y}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка ввода: " + ex.Message);
+            }
+        }
+
+        private void btnSolve3x3_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                double a1 = Convert.ToDouble(txtA1_3x3.Text);
+                double b1 = Convert.ToDouble(txtB1_3x3.Text);
+                double c1 = Convert.ToDouble(txtC1_3x3.Text);
+                double d1 = Convert.ToDouble(txtD1_3x3.Text);
+
+                double a2 = Convert.ToDouble(txtA2_3x3.Text);
+                double b2 = Convert.ToDouble(txtB2_3x3.Text);
+                double c2 = Convert.ToDouble(txtC2_3x3.Text);
+                double d2 = Convert.ToDouble(txtD2_3x3.Text);
+
+                double a3 = Convert.ToDouble(txtA3_3x3.Text);
+                double b3 = Convert.ToDouble(txtB3_3x3.Text);
+                double c3 = Convert.ToDouble(txtC3_3x3.Text);
+                double d3 = Convert.ToDouble(txtD3_3x3.Text);
+
+                double det = a1 * (b2 * c3 - c2 * b3) - b1 * (a2 * c3 - c2 * a3) + c1 * (a2 * b3 - b2 * a3);
+
+                if (det == 0)
+                {
+                    MessageBox.Show("Система не имеет единственного решения.");
+                    return;
+                }
+
+                double x = (d1 * (b2 * c3 - c2 * b3) - b1 * (d2 * c3 - c2 * d3) + c1 * (d2 * b3 - b2 * d3)) / det;
+                double y = (a1 * (d2 * c3 - c2 * d3) - d1 * (a2 * c3 - c2 * a3) + c1 * (a2 * d3 - d2 * a3)) / det;
+                double z = (a1 * (b2 * d3 - d2 * b3) - b1 * (a2 * d3 - d2 * a3) + d1 * (a2 * b3 - b2 * a3)) / det;
+
+                lblResult3x3.Text = $"x = {x}, y = {y}, z = {z}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка ввода: " + ex.Message);
+            }
+        }
+        private void btnClose_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}  
+        
